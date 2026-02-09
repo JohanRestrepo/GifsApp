704643ab3ca3fa4046a7c7463b0d8f698a5f0496
Zarigueya <esnic1994@gmail.com>
2026-02-09 00:55:08 -0500

    agregue variables de entorno y alias

--- src/app/gifs/components/gifs-side-menu/gifs-side-menu-header/gifs-side-menu-header.component.ts
+++ src/app/gifs/components/gifs-side-menu/gifs-side-menu-header/gifs-side-menu-header.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { environment } from '@environments/environment';//Esto lo cree con un alias en tsconfig.json
 
 @Component({
   selector: 'gifs-side-menu-header',
   imports: [],
   templateUrl: './gifs-side-menu-header.component.html',
 })
-export class GifsSideMenuHeader { }
+export class GifsSideMenuHeader { 
+
+  envs = environment
+}
--- src/app/gifs/components/gifs-side-menu/gifs-side-menu-options/gifs-side-menu-options.component.ts
+++ src/app/gifs/components/gifs-side-menu/gifs-side-menu-options/gifs-side-menu-options.component.ts
@@ -1,8 +1,33 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+interface MenuOption{
+  icon: string;
+  label: string;
+  route: string;
+  subLabel: string;
+}
 
 @Component({
   selector: 'gifs-side-menu-options',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './gifs-side-menu-options.component.html',
 })
-export class GifsSideMenuOptions { }
+export class GifsSideMenuOptions { 
+
+  menuOptions: MenuOption[] = [
+    {
+      icon: 'fa-solid fa-chart-line',
+      label: 'Trending',
+      subLabel: 'Gifs populares',
+      route: '/dashboard/trending'
+    },
+    {
+      icon: 'fa-solid fa-magnifying-glass',
+      label: 'Buscador',
+      subLabel: 'Buscar gifs',
+      route: '/dashboard/search'
+    }
+  ]
+
+}
