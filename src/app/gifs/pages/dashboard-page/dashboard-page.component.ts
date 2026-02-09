82e83c985920bf26d618d0c2f5bf2879bcfddca1
Zarigueya <esnic1994@gmail.com>
2026-02-09 00:01:56 -0500

    Separe el html en componentes pequeños

--- src/app/gifs/pages/dashboard-page/dashboard-page.component.ts
+++ src/app/gifs/pages/dashboard-page/dashboard-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { GifsSideMenu } from '../../components/gifs-side-menu/gifs-side-menu.component';
 
 @Component({
   selector: 'app-dashboard-page',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, GifsSideMenu],
   templateUrl: './dashboard-page.component.html',
 })
 export default class DashboardPageComponent { }
